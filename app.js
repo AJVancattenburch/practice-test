40d994fec885dfda8210038b2e95f49a5c8fabed
AJVancattenburch <wanderwithme1988@gmail.com>
2023-03-19 21:24:22 -0600

    practice-test-final

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const ageToVote = 18
+const ageToVote = 100
 
 const user = {
   name: 'Sherlock Holmes',
@@ -13,9 +13,12 @@ const cases = [
   { title: 'The Adventure of the Empty House', releaseYear: 1903 },
   { title: 'The Adventure of the Norwood Builder', releaseYear: 1903 },
   { title: 'The Adventure of the Dancing Men', releaseYear: 1903 },
-  { title: 'The Adventure of the Solitary Cyclist', releaseYear: 1904 }
-]
+  { title: 'The Adventure of the Solitary Cyclist', releaseYear: 1904 },
+];
 
-// YOUR JS CODE
-
-
+let newCases = cases.map((title) => title);
+for(let i = 0; i < cases.length; i++){
+  if (cases[i].releaseYear === 1903){
+  console.log(cases[i].title)
+  };
+}
