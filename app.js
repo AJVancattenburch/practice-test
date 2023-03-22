ece109d457d634e9c1d028efe24bb365eb6bfa7b
AJVancattenburch <wanderwithme1988@gmail.com>
2023-03-22 13:33:15 -0600

    Practice test further revision and practice

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const ageToVote = 100
+const ageToVote = 18
 
 const user = {
   name: 'Sherlock Holmes',
@@ -14,11 +14,50 @@ const cases = [
   { title: 'The Adventure of the Norwood Builder', releaseYear: 1903 },
   { title: 'The Adventure of the Dancing Men', releaseYear: 1903 },
   { title: 'The Adventure of the Solitary Cyclist', releaseYear: 1904 },
-];
+]
 
-let newCases = cases.map((title) => title);
-for(let i = 0; i < cases.length; i++){
-  if (cases[i].releaseYear === 1903){
-  console.log(cases[i].title)
-  };
+
+//2. **Creating and using variables in JavaScript**
+
+let currentYear = 2023
+let jsReleaseYear = 1995
+let jsAge = currentYear - jsReleaseYear
+  console.log(jsAge)
+
+  if (ageToVote >= jsAge)
+  console.log('Javascript is too young to vote')
+    else(
+      console.log('Javascript is old enough to vote')
+    )
+
+
+//3. **Creating and using Loops in JavaScript**
+
+for(let i = 0; i <= 100; i ++){
+  console.log(i)
+}
+
+console.log('Starting')
+for(let i = 0; i <= 100; i += 10){
+  console.log(i)
+  if(i == 50)
+  console.log('halfway')
+  if(i === 100)
+  console.log('Finished')
+
+}
+
+
+//4. **Accessing properties on an object in JavaScript**
+
+console.log(user.name, 'lives at ', user.address)
+
+
+//5. **Working with an array of objects in JavaScript**
+
+
+for(let i = 0; i <= cases.length; i++){
+  let eachCase = cases[i]
+  if(eachCase.releaseYear == 1903)
+  console.log(cases[i])
 }
